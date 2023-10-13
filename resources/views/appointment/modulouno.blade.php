2963d38b33d1355c15e1a849d70b605cd8db759c
fredual <fredual16@gmail.com>
2023-10-12 22:31:26 -0500

    citas con collapse

--- resources/views/appointment/modulouno.blade.php
+++ resources/views/appointment/modulouno.blade.php
@@ -1,20 +1,21 @@
 <div class="table-responsive">
     <h2 class="text-center">Horarios de la Semana Actual</h2>
-    <table id="tablaConsultas" class="table table-bordered table-striped">
+    <table class="table table-bordered table-striped">
         <thead class="bg-default text-white text-center">
-            <tr>
-                <th>Hora</th>
-                <th id="date1"></th>
-                <th id="date2"></th>
-                <th id="date3"></th>
-                <th id="date4"></th>
-                <th id="date5"></th>
-                <th id="date6"></th>
-                <th id="date7"></th>
-            </tr>
+          <tr>
+            <th>Hora</th>
+            <th id="date1"></th>
+            <th id="date2"></th>
+            <th id="date3"></th>
+            <th id="date4"></th>
+            <th id="date5"></th>
+            <th id="date6"></th>
+            <th id="date7"></th>
+          </tr>
         </thead>
         <tbody class="text-center" id="tbodyUno">
-            <!-- Aquí generaremos las filas de horas dinámicamente -->
+          <!-- Aquí generaremos las filas de horas dinámicamente -->
         </tbody>
-    </table>
+      </table>
+      
 </div>
